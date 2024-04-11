45cefcdc40a5ccad6177439c68833ab38ed6ad5e
menni_diskind <menni.diskind@gmail.com>
2024-04-11 13:41:59 +0300

    new exercises

--- python/exercises/ex 09.04.24/es division/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ python/exercises/ex 09.04.24/es division/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -6,6 +6,6 @@ num = num // 10
 x3 = num
 sumOfStones = x1+x2+x3
 print("The number of stones collected by all the pigs is:", sumOfStones)
-print("If they divide the stones equally, each will get:"+ sumOfStones // 3)
+print("If they divide the stones equally, each will get:", sumOfStones // 3)
 print("The remainder of the division of the stones is:", sumOfStones % 3)
-print((sumOfStones % 3) == False) 
+print((sumOfStones % 3) == False)
