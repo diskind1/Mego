b0487d4575f57acfb16d176ee7abf2301bcc7033
menni_diskind <menni.diskind@gmail.com>
2024-04-11 16:58:08 +0300

    add a line in the project

--- python/projects/self.py - 1/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ python/projects/self.py - 1/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -66,6 +66,7 @@ print("""picture 7:
     """)
 
 The_players_guess=input("Guess a letter: ")
+The_players_guess = The_players_guess.lower()
 print(The_players_guess)
 
 
@@ -83,8 +84,15 @@ print(The_players_guess)
 
 
 
-# import random
-# print("""Welcome to the game Hangman
+
+
+
+
+
+
+# import random 
+
+# HANGMAN_ASCII_ART="""Welcome to the game Hangman
 #  _    _
 # | |  | |
 # | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
@@ -93,7 +101,9 @@ print(The_players_guess)
 # |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
 #                      __/ |
 #                     |___/
-#         """ )
+#         """
+# MAX_TRIES=6
+# print(HANGMAN_ASCII_ART, MAX_TRIES)        
 # print (random.randint(5,10))
 # print("""picture 1:
 #     x-------x
@@ -146,3 +156,6 @@ print(The_players_guess)
 #     |      / \\
 #     |
 #     """)
+
+# The_players_guess=input("Guess a letter: ")
+# print(The_players_guess)
