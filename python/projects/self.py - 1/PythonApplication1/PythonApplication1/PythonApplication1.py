710b2ea9e3e3e08fb2ed0d42839e09670f127226
menni_diskind <menni.diskind@gmail.com>
2024-04-14 18:58:53 +0300

    continue the project

--- python/projects/self.py - 1/PythonApplication1/PythonApplication1/PythonApplication1.py
+++ python/projects/self.py - 1/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -70,6 +70,17 @@ The_players_guess = The_players_guess.lower()
 print(The_players_guess)
 
 
+def validate_input(The_players_guess):
+    if len(The_players_guess) > 1:
+        return "E1"
+    elif not The_players_guess.isalpha():
+        return "E2"
+    elif not The_players_guess.islower():
+        return "E3"
+    else:
+        return The_players_guess
+result = validate_input(The_players_guess)
+print(result)
 
 
 
@@ -90,72 +101,75 @@ print(The_players_guess)
 
 
 
-# import random 
 
-# HANGMAN_ASCII_ART="""Welcome to the game Hangman
-#  _    _
-# | |  | |
-# | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
-# |  __  |/ _' | '_ \ / _' | '_ ' _ \ / _' | '_ \\
-# | |  | | (_| | | | | (_| | | | | | | (_| | | | |
-# |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-#                      __/ |
-#                     |___/
-#         """
-# MAX_TRIES=6
-# print(HANGMAN_ASCII_ART, MAX_TRIES)        
-# print (random.randint(5,10))
-# print("""picture 1:
-#     x-------x
-#     """)
-# print("""picture 2:
-#     x-------x
-#     |
-#     |
-#     |
-#     |
-#     |
-# """)
-# print("""picture 3:
-#     x-------x
-#     |       |
-#     |       0
-#     |
-#     |
-#     |
-#     """)
-# print("""picture 4:
-#     x-------x
-#     |       |
-#     |       0
-#     |       |
-#     |
-#     |
-#     """)
-# print("""picture 5:
-#     x-------x
-#     |       |
-#     |       0
-#     |      /|\\
-#     |
-#     |
-#     """)
-# print("""picture 6:
-#     x-------x
-#     |       |
-#     |       0
-#     |      /|\\
-#     |      /
-#     |
-#     """)
-# print("""picture 7:
-#     x-------x
-#     |       |
-#     |       0
-#     |      /|\\
-#     |      / \\
-#     |
-#     """)
 
-# The_players_guess=input("Guess a letter: ")
-# print(The_players_guess)
+# # # # import random 
+
+# # # # HANGMAN_ASCII_ART="""Welcome to the game Hangman
+# # # #  _    _
+# # # # | |  | |
+# # # # | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
+# # # # |  __  |/ _' | '_ \ / _' | '_ ' _ \ / _' | '_ \\
+# # # # | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+# # # # |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+# # # #                      __/ |
+# # # #                     |___/
+# # # #         """
+# # # # MAX_TRIES=6
+# # # # print(HANGMAN_ASCII_ART, MAX_TRIES)        
+# # # # print (random.randint(5,10))
+# # # # print("""picture 1:
+# # # #     x-------x
+# # # #     """)
+# # # # print("""picture 2:
+# # # #     x-------x
+# # # #     |
+# # # #     |
+# # # #     |
+# # # #     |
+# # # #     |
+# # # # """)
+# # # # print("""picture 3:
+# # # #     x-------x
+# # # #     |       |
+# # # #     |       0
+# # # #     |
+# # # #     |
+# # # #     |
+# # # #     """)
+# # # # print("""picture 4:
+# # # #     x-------x
+# # # #     |       |
+# # # #     |       0
+# # # #     |       |
+# # # #     |
+# # # #     |
+# # # #     """)
+# # # # print("""picture 5:
+# # # #     x-------x
+# # # #     |       |
+# # # #     |       0
+# # # #     |      /|\\
+# # # #     |
+# # # #     |
+# # # #     """)
+# # # # print("""picture 6:
+# # # #     x-------x
+# # # #     |       |
+# # # #     |       0
+# # # #     |      /|\\
+# # # #     |      /
+# # # #     |
+# # # #     """)
+# # # # print("""picture 7:
+# # # #     x-------x
+# # # #     |       |
+# # # #     |       0
+# # # #     |      /|\\
+# # # #     |      / \\
+# # # #     |
+# # # #     """)
+
+# # # # The_players_guess=input("Guess a letter: ")
+# # # # The_players_guess = The_players_guess.lower()
+# # # # print(The_players_guess)
