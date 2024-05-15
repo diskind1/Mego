970c8149b53187ddbaeda136437cc1a63b889997
menni_diskind <menni.diskind@gmail.com>
2024-05-15 17:09:02 +0300

    a new fullstack exercises

---
+++ C#/exercises/ex 130524/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static int F(int x)
+        {
+            if (x > 0)
+                return 1 + F(x / 10);
+            return 0;
+        }
+        /*
+        static int CountEvensDigits(int x)  //  x=0
+        {
+            if (x > 0)
+                if (x % 2 == 0)
+                {
+                    return 1 + F(x / 10);
+                }
+                else
+                {
+                    return 0 + F(x / 10);
+                }
+            return 0;
+        }        
+        static int CountEvensDigits(int x)  //  x=5
+        {
+            if (x > 0)
+                if (x % 2 == 0)
+                {
+                    return 1 + F(x / 10);
+                }
+                else
+                {
+                    return 0 + F(x / 10);   //  0 + ??? =   0+0 =   0
+                }
+            return 0;
+        }
+        */
+        static int CountEvensDigits(int x)  //  52
+        {
+            if (x > 0)
+                if (x % 2 == 0)
+                {
+                    return 1 + F(x / 10);   //  1 + ??? =   1+0 =   1
+                }
+                else
+                {
+                    return 0 + F(x / 10);
+                }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountEvensDigits(52));
+        }
+    }
+}
---
+++ python/exercises/ex 130524/PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,26 @@
+# # # def F(x):   #   x=0
+# # #     if(x>0):
+# # #         return 1+F(x//10)
+# # #     return 0
+# # # def F(x):   #   x=4
+# # #     if(x>0):
+# # #         return 1+F(x//10)   #   1 + ???     1+0 =   1
+# # #     return 0
+# # # def F(x):   #   x=46
+# # #     if(x>0):
+# # #         return 1+F(x//10)   #   1 +???      1+1 =   2
+# # #     return 0
+
+# # def F(x):   #   x=469
+# #     if(x>0):
+# #         return 1+F(x//10)   #   1 + ???     1+2 =   3
+# #     return 0
+# # print(F(469))
+
+# # # def F(x):
+# # #     total_sum = 0
+# # #     while x > 0:
+# # #         total_sum+=1
+# # #         x //= 10
+# # #     return total_sum
+# # # print(F(469))
